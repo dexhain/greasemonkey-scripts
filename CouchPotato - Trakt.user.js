6b48196cf4d59e41b6cae2ae555987a45d54d2d7
dexhain <dexhain+github@gmail.com>
2014-12-01 14:54:25 +0000

    Update CouchPotato - Trakt.user.js

--- CouchPotato - Trakt.user.js
+++ CouchPotato - Trakt.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/dexhain/greasemonkey-scripts
 // @include	entercouchaddress
 // @downloadURL https://github.com/dexhain/greasemonkey-scripts/raw/master/CouchPotato%20-%20Trakt.user.js
-// @version     1.0
+// @version     1.1
 // @grant       GM_xmlhttpRequest
 // @grant		GM_addStyle
 // @grant		GM_setValue
@@ -37,45 +37,53 @@ if(window.top == window.self)
 			width: 25px !important;\
 		}\
 	");
-	var watchlist = [];
-	var collection = [];
-	GM_xmlhttpRequest({
-		method: "GET",
-		url: "http://api.trakt.tv/user/library/movies/collection.json/" + GM_getValue("apikey") + "/" + GM_getValue("username") + "/min",
-		onload: function(response){
-			var json = JSON.parse(response.responseText);
-			for(var i = 0; i < json.length; i++)
-				collection.push(json[i].imdb_id);
-		}
-	});
-	GM_xmlhttpRequest({
-		method: "GET",
-		url: "http://api.trakt.tv/user/watchlist/movies.json/" + GM_getValue("apikey") + "/" + GM_getValue("username"),
-		onload: function(response){
-			var json = JSON.parse(response.responseText);
-			for(var i = 0; i < json.length; i++)
-				watchlist.push(json[i].imdb_id);
-		}
-	});
 	window.addEventListener('load', function() {
-		var mr = document.getElementsByClassName("media_result");
-		for(var i = 0; i < mr.length; i++)
+		var watchlist = [];
+		var collection = [];
+		GM_xmlhttpRequest({
+			method: "GET",
+			url: "http://api.trakt.tv/user/library/movies/collection.json/" + GM_getValue("apikey") + "/" + GM_getValue("username") + "/min",
+			onload: function(response){
+				var json = JSON.parse(response.responseText);
+				for(var i = 0; i < json.length; i++)
+					collection.push(json[i].imdb_id);
+				var mr = document.getElementsByClassName("media_result");
+				for(var i = 0; i < mr.length; i++)
+				{
+					if(collection.indexOf(mr[i].id) > -1)
+					{
+						var overlay = document.createElement("div");
+						overlay.classList.add("collection");
+						mr[i].appendChild(overlay);
+					}
+				}
+			}
+		});
+		GM_xmlhttpRequest({
+			method: "GET",
+			url: "http://api.trakt.tv/user/watchlist/movies.json/" + GM_getValue("apikey") + "/" + GM_getValue("username"),
+			onload: function(response){
+				var json = JSON.parse(response.responseText);
+				for(var i = 0; i < json.length; i++)
+					watchlist.push(json[i].imdb_id);
+				var mr = document.getElementsByClassName("media_result");
+				for(var i = 0; i < mr.length; i++)
+				{
+					if(watchlist.indexOf(mr[i].id) > -1)
+					{
+						var overlay = document.createElement("div");
+						overlay.classList.add("watchlist");
+						mr[i].appendChild(overlay);
+					}
+				}
+			}
+		});
+		var mres = document.getElementsByClassName("media_result");
+		for(var i = 0; i < mres.length; i++)
 		{
-			if(watchlist.indexOf(mr[i].id) > -1)
-			{
-				var overlay = document.createElement("div");
-				overlay.classList.add("watchlist");
-				mr[i].appendChild(overlay);
-			}
-			if(collection.indexOf(mr[i].id) > -1)
-			{
-				var overlay = document.createElement("div");
-				overlay.classList.add("collection");
-				mr[i].appendChild(overlay);
-			}
-			var imdb = mr[i].getElementsByClassName("imdb")[0];
+			var imdb = mres[i].getElementsByClassName("imdb")[0];
 			var traktlink = document.createElement("a");
-			traktlink.href = "http://trakt.tv/search/movies?q=" + mr[i].id;
+			traktlink.href = "http://trakt.tv/search/movies?q=" + mres[i].id;
 			traktlink.target = "_blank";
 			traktlink.innerHTML = "Trakt";
 			traktlink.className = "action icon2";
